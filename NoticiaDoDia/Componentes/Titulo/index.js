36d2242884fbafa4110d719808627665d959d133
Vinícius Sidrim <viniciussidrim99@gmail.com>
2021-03-04 10:52:28 -0300

    .

---
+++ NoticiaDoDia/Componentes/Titulo/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Titulo() {
+  return (
+    <View>
+      <Text>Notícia do dia</Text>
+    </View>
+  );
+}
